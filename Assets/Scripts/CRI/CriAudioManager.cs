71795a57abde80eb62360d6326ddb6ec2f1397bb
AkiraWatanabe23 <a0323w@gmail.com>
2023-10-11 18:36:22 +0900

    [add] CRIMW導入

---
+++ Assets/Scripts/CRI/CriAudioManager.cs
@@ -0,0 +1,48 @@
+﻿using CriWare;
+
+/// <summary> CRIを用いたAudioManager </summary>
+public class CriAudioManager
+{
+    private static CriAudioManager _instance = default;
+
+    //音源再生用のクラス
+    private readonly CriAtomExPlayer _criBGMPlayer = new();
+    private readonly CriAtomExPlayer _criSEPlayer = new();
+
+
+    public static CriAudioManager Instance
+    {
+        get
+        {
+            _instance ??= new();
+            return _instance;
+        }
+    }
+
+    private CriAudioManager()
+    {
+
+    }
+
+    public void PlayBGM(string cueSheetName, string cueName)
+    {
+        //どの音を再生するか選択する
+        var acb = CriAtom.GetCueSheet(cueSheetName).acb;
+        _criBGMPlayer.SetCue(acb, cueName);
+
+        _criBGMPlayer.Start();
+    }
+
+    public void PlaySE(string cueSheetName, string cueName)
+    {
+        //どの音を再生するか選択する
+        var acb = CriAtom.GetCueSheet(cueSheetName).acb;
+        _criSEPlayer.SetCue(acb, cueName);
+
+        _criSEPlayer.Start();
+    }
+
+    public void StopBGM() { }
+
+    public void StopSE() { }
+}
