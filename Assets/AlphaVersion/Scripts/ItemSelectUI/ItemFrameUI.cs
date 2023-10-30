be5362a991ab22af649ca36db5abd2e66a376a62
SatouTappei <stutpi.signup@gmail.com>
2023-10-30 19:21:00 +0900

    選択したアイテムを反映する処理を追加

---
+++ Assets/AlphaVersion/Scripts/ItemSelectUI/ItemFrameUI.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Alpha
+{
+    /// <summary>
+    /// 選択するアイテムそれぞれのアイコンUIのクラス
+    /// </summary>
+    [RequireComponent(typeof(CanvasGroup))]
+    public class ItemFrameUI : MonoBehaviour
+    {
+        [SerializeField] Image _icon;
+        [SerializeField] Image _frame;
+        [SerializeField] CanvasGroup _canvasGroup;
+
+        /// <summary>
+        /// このUIに割り当てるアイコンを設定して有効化
+        /// </summary>
+        public void Valid(Sprite icon)
+        {
+            _icon.sprite = icon;
+            Deselect();
+        }
+
+        /// <summary>
+        /// このUIに対応するアイテムが無いので無効化
+        /// </summary>
+        public void InValid()
+        {
+            _canvasGroup.alpha = 0;
+        }
+
+        /// <summary>
+        /// このアイテムを選択した
+        /// </summary>
+        public void Select()
+        {
+            _frame.color = Color.red;
+        }
+
+        /// <summary>
+        /// 他のアイテムを選択した
+        /// </summary>
+        public void Deselect()
+        {
+            _frame.color = Color.black;
+        }
+    }
+}
